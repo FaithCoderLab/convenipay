e84c51d5ab6098e0e9acd103b6ebd7af2eb968bf
FaithCoderLab <seinplan97@gmail.com>
2025-02-18 13:46:35 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/convenipay/ConvenipayApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.convenipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvenipayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvenipayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/convenipay/ConvenipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.convenipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenipayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
